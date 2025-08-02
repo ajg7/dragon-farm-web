6a2fa43e9813c60eb589e4654e25f7902ff578be
AJ Gebara <ajgebara@gmail.com>
2025-08-02 12:21:49 -0500

    lets start

--- src/pages/AuthValidation.tsx
+++ src/pages/AuthValidation.tsx
@@ -2,4 +2,17 @@ const AuthValidation = () => {
   return <div></div>;
 };
 
+/**
+ * We need a header1, header2, header3
+ * We need a card
+ * We need an input
+ * We need a button
+ * We need a Text
+ * We need a Checkbox
+ * We need an Icon
+ * We need a Link
+ * We need a Separator
+ * We need a Form
+ */
+
 export default AuthValidation;
